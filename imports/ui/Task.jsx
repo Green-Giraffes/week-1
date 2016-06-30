83e0a656afba6b9b0a6266338b6575e77f0633c0
Dev Eggers <eggers.brendan@gmail.com>
2016-06-29 23:49:11 -0400

    Commits working Todo App

---
+++ imports/ui/Task.jsx
@@ -0,0 +1,40 @@
+import React, { Component, PropTypes } from 'react';
+
+import {Tasks} from '../api/tasks.js';
+
+//Task Component, creates a single task
+export default class Task extends Component {
+  toggleChecked() {
+    // Set the checked property to the opposite of its current value
+    Tasks.update(this.props.task._id, {
+      $set: { checked: !this.props.task.checked },
+    });
+  }
+
+  deleteThisTask() {
+    Tasks.remove(this.props.task._id);
+  }
+  render() {
+    const taskClassName = this.props.task.checked ? 'checked':'';
+    return (
+      <li className={taskClassName}>
+        <button className="delete" onClick={this.deleteThisTask.bind(this)}>
+          &times;
+        </button>
+
+        <input
+          type="checkbox"
+          readOnly
+          checked={this.props.task.checked}
+          onClick={this.toggleChecked.bind(this)}
+        />
+
+        <span className="text">{this.props.task.text}</span>
+      </li>
+    );
+  }
+}
+
+Task.propTypes = {
+  task: PropTypes.object.isRequired,
+};
