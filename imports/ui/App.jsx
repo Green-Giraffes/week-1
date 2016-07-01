faa781dbd4358149ae5bc8e8007775dd5391bf68
Donald Donovan <don.sequitur@gmail.com>
2016-07-01 04:29:59 +0000

    Add temporary ui state

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -8,6 +8,15 @@ import Task from './Task.jsx';
 
 //app component - represents the whole app
 class App extends Component {
+  
+  constructor(props) {
+    super(props);
+ 
+    this.state = {
+      hideCompleted: false,
+    };
+  }
+  
   handleSubmit(event){
     event.preventDefault();
 
@@ -19,8 +28,21 @@ class App extends Component {
 
     ReactDOM.findDOMNode(this.refs.textInput).value = '';
   }
+  
+  toggleHideCompleted() {
+    this.setState({
+      hideCompleted: !this.state.hideCompleted,
+    });
+  }
+  
   renderTasks() {
-    return this.props.tasks.map((task) => (
+    let filteredTasks = this.props.tasks;
+    
+    if (this.state.hideCompleted) {
+      filteredTasks = filteredTasks.filter(task => !task.checked);
+    }
+    
+    return filteredTasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
@@ -29,15 +51,27 @@ class App extends Component {
     return (
       <div className='container'>
         <header>
-          <h1>Todo List</h1>
+          <h1>Todo List ({this.props.incompleteCount})</h1>
+          
+          <label className="hide-completed">
+            <input
+              type="checkbox"
+              readOnly
+              checked={this.state.hideCompleted}
+              onClick={this.toggleHideCompleted.bind(this)}
+            />
+            Hide Completed Tasks
+          </label>
+          
+          <form className='new-task' onSubmit={this.handleSubmit.bind(this)}>
+            <input
+              type='text'
+              ref='textInput'
+              placeholder='Type to add new tasks'
+            />
+          </form>
+          
         </header>
-        <form className='new-task' onSubmit={this.handleSubmit.bind(this)}>
-          <input
-            type='text'
-            ref='textInput'
-            placeholder='Type to add new task'
-          />
-        </form>
         <ul>
           {this.renderTasks()}
         </ul>
@@ -48,10 +82,12 @@ class App extends Component {
 
 App.propTypes = {
   tasks: PropTypes.array.isRequired,
+  incompleteCount: PropTypes.number.isRequired,
 };
 
 export default createContainer(() => {
   return {
     tasks: Tasks.find({}, {sort: {createdAt: -1} }).fetch(),
+    incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
   };
 }, App);
